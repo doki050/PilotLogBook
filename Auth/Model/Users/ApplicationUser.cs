6a724e9a9c76fca51bf862f11a2affe90e855a11
Kovács Benjamin Márk <kovacs.benjamin.mark99@gmail.com>
2024-09-14 19:37:23 +0200

    Before auth

---
+++ Auth/Model/Users/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Model.Users;
+
+public class ApplicationUser : IdentityUser
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
